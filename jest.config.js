ca267f3812350cbd30cebc20bd1f47b83e979c94
Kaelan <kaelan.stephens@neofinancial.com>
2021-10-16 14:08:12 -0600

    updated dependencies

--- jest.config.js
+++ jest.config.js
@@ -8,7 +8,7 @@ module.exports = {
   ],
   globals: {
     'ts-jest': {
-      tsConfig: '<rootDir>/test/tsconfig.json'
+      tsconfig: '<rootDir>/test/tsconfig.json'
     }
   },
   preset: 'ts-jest',
